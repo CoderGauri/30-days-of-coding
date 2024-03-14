90c1a24b3068213b803238fe096f663f01395494
CoderGauri <gauriM304@gmail.com>
2024-03-14 19:58:10 +0530

    variables

--- Js-Revise/first/script.js
+++ Js-Revise/first/script.js
@@ -6,3 +6,11 @@ b = undefined;
 doesFollow = true;
 fullName = 24;
 console.log(fullName);
+
+var name = "Gauri";
+name = "Mahashabde";
+console.log(name);
+let greet = "Hello";
+console.log(greet);
+const greetAgain = "Good Morning";
+console.log(greetAgain);
