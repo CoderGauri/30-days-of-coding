1472f6e32004035ba19691d26155c40a22efa6f7
CoderGauri <gauriM304@gmail.com>
2024-03-14 20:03:34 +0530

    variables

--- Js-Revise/first/script.js
+++ Js-Revise/first/script.js
@@ -1,16 +1,29 @@
-fullName = "Gauri Mahashabde";
-age = 25;
-price = 120.89;
-a = null;
-b = undefined;
-doesFollow = true;
-fullName = 24;
-console.log(fullName);
+// fullName = "Gauri Mahashabde";
+// age = 25;
+// price = 120.89;
+// a = null;
+// b = undefined;
+// doesFollow = true;
+// fullName = 24;
+// console.log(fullName);
 
-var name = "Gauri";
-name = "Mahashabde";
-console.log(name);
-let greet = "Hello";
-console.log(greet);
-const greetAgain = "Good Morning";
-console.log(greetAgain);
+// var name = "Gauri";
+// name = "Mahashabde";
+// console.log(name);
+// let greet = "Hello";
+// greet = "Hey"
+// console.log(greet);
+// const greetAgain = "Good Morning";
+// greetAgain = "Good evening"
+// console.log(greetAgain);
+
+console.log(game);
+var game; //undefine hoisted
+game = "tennis"
+
+//console.log(show);
+let show;
+
+//onsole.log(movie);
+//const movie;
+
