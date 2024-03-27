1da301af9c97b57181c4543d3e9d7e4a2358ae0d
CoderGauri <gauriM304@gmail.com>
2024-03-27 19:09:32 +0530

    Hoisting

--- Js-Revise/Hoisting/hoiting.js
+++ Js-Revise/Hoisting/hoiting.js
@@ -2,3 +2,21 @@
 x = 5 
 var x;
 console.log(x);
+
+carName2 = "Volvo";
+var carName2;
+console.log(carName2);
+
+
+carName = "Volvo";
+//let carName;
+console.log(carName);
+
+carName1 = "Volvo";
+//const carName1;
+console.log(carName1);
+
+console.log(singer);
+var singer;
+singer = "sonu";
+
