14ad14ef7ccaa407370666521a52134a73b4392b
CoderGauri <gauriM304@gmail.com>
2024-04-05 19:05:14 +0530

    Data types

--- Js-Revise/Data-Types/script.js
+++ Js-Revise/Data-Types/script.js
@@ -1,1 +1,19 @@
 //data type
+//Data types in JavaScript refer to the attribute associated with the kind of data we are storing or working on.
+//primitive
+//defined and most basic type
+//number 
+var num = 1;
+console.log(num);
+//string type
+var name = "Gauri Mahashabde";
+var name1 ='Gauri Mahashabde';
+var name3 = `Gauri Mahashabde`;
+console.log(name);
+console.log(name1);
+console.log(name3);
+//boolean 
+var dofollow = true;
+var dontfollow = false;
+console.log(dofollow);
+console.log(dontfollow);
